b5340fdd87cbfb34357e74268e3fa74c109c357c
Vlad.Zherko <vladikon19@tut.by>
2024-07-02 00:25:51 +0300

    added : start method

--- src/main/java/ru/clevertec/check/CheckRunner.java
+++ src/main/java/ru/clevertec/check/CheckRunner.java
@@ -1,7 +1,11 @@
 package ru.clevertec.check;
 
+import ru.clevertec.check.factory.ServiceFactory;
+import ru.clevertec.check.service.StringInputService;
+
 public class CheckRunner {
     public static void main(String[] args) {
-        System.out.println("hello!");
+        BillApplication billApplication = new BillApplication(new StringInputService(), ServiceFactory.buildClientService());
+        billApplication.start(args);
     }
 }
