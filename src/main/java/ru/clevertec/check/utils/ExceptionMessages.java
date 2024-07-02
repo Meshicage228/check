e90062e07d22e1d13948be1064a1855311f3b9d1
Vlad.Zherko <vladikon19@tut.by>
2024-07-02 23:04:18 +0300

    init

---
+++ src/main/java/ru/clevertec/check/utils/ExceptionMessages.java
@@ -0,0 +1,9 @@
+package ru.clevertec.check.utils;
+
+public class ExceptionMessages {
+    private ExceptionMessages(){
+    }
+    public static String NOT_ENOUGH_MONEY = "NOT ENOUGH MONEY";
+    public static String INTERNAL_SERVER_ERROR = "INTERNAL SERVER ERROR";
+    public static String BAD_REQUEST = "BAD REQUEST";
+}
