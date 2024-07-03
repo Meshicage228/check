88e4a138bf19b296907d0ef48f8178afcca97054
Vlad.Zherko <vladikon19@tut.by>
2024-07-03 13:41:17 +0300

    added : factory method

--- src/main/java/ru/clevertec/check/CheckRunner.java
+++ src/main/java/ru/clevertec/check/CheckRunner.java
@@ -1,13 +1,10 @@
 package ru.clevertec.check;
 
 import ru.clevertec.check.factory.ServiceFactory;
-import ru.clevertec.check.service.BillService;
-import ru.clevertec.check.service.StringInputService;
-import ru.clevertec.check.utils.FileService;
 
 public class CheckRunner {
     public static void main(String[] args) {
-        BillApplication billApplication = new BillApplication(new StringInputService(), ServiceFactory.buildClientService(), new BillService(new FileService()), new FileService());
+        BillApplication billApplication = ServiceFactory.createBillApplication();
         billApplication.start(args);
     }
 }
--- src/main/java/ru/clevertec/check/factory/ServiceFactory.java
+++ src/main/java/ru/clevertec/check/factory/ServiceFactory.java
@@ -2,13 +2,45 @@ package ru.clevertec.check.factory;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
-import ru.clevertec.check.service.CardService;
-import ru.clevertec.check.service.ClientService;
-import ru.clevertec.check.service.ProductService;
+import ru.clevertec.check.BillApplication;
+import ru.clevertec.check.service.*;
+import ru.clevertec.check.service.impl.*;
+import ru.clevertec.check.utils.StringInputUtil;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ServiceFactory {
-    public static ClientService buildClientService() {
-        return new ClientService(new ProductService(), new CardService());
+
+    public static StringInputUtil createStringInputUtil() {
+        return new StringInputUtil();
+    }
+
+    public static ProductService createProductService() {
+        return new ProductServiceImpl();
+    }
+
+    public static CardService createCardService() {
+        return new CardServiceImpl();
+    }
+
+    public static ClientService createClientService() {
+        ProductService productService = createProductService();
+        CardService cardService = createCardService();
+        return new ClientServiceImpl(productService, cardService);
+    }
+
+    public static FileService createFileService() {
+        return new FileServiceImpl();
+    }
+
+    public static BillService createBillService() {
+        FileService fileService = createFileService();
+        return new BillServiceImpl(fileService);
+    }
+
+    public static BillApplication createBillApplication() {
+        StringInputUtil stringInputUtil = createStringInputUtil();
+        ClientService clientService = createClientService();
+        BillService billService = createBillService();
+        return new BillApplication(stringInputUtil, clientService, billService);
     }
 }
