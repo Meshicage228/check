e19d16a159fc60a008786fd858a437a0c8af8792
Vlad.Zherko <vladikon19@tut.by>
2024-07-08 00:25:37 +0300

    exception rework

--- src/main/java/ru/clevertec/check/exceptions/BadRequestException.java
+++ src/main/java/ru/clevertec/check/exceptions/BadRequestException.java
@@ -1,24 +1,4 @@
 package ru.clevertec.check.exceptions;
 
-import ru.clevertec.check.utils.ExceptionMessages;
-
-public class BadRequestException extends AbstractErrorFileWriter{
-
-    public BadRequestException() {
-        createErrorFile(getErrorMessage());
-    }
-
-    public BadRequestException(String errorFilePath) {
-        createErrorFile(getErrorMessage(), errorFilePath);
-    }
-
-    @Override
-    public String getErrorMessage() {
-        return ExceptionMessages.BAD_REQUEST;
-    }
-
-    @Override
-    public void createErrorFile(String errorMessage) {
-        super.createErrorFile(errorMessage);
-    }
+public class BadRequestException extends Exception {
 }
--- src/main/java/ru/clevertec/check/exceptions/NotEnoughMoneyException.java
+++ src/main/java/ru/clevertec/check/exceptions/NotEnoughMoneyException.java
@@ -1,20 +1,4 @@
 package ru.clevertec.check.exceptions;
 
-import ru.clevertec.check.utils.ExceptionMessages;
-
-public class NotEnoughMoneyException extends AbstractErrorFileWriter {
-
-    public NotEnoughMoneyException() {
-        createErrorFile(getErrorMessage());
-    }
-
-    @Override
-    public String getErrorMessage() {
-        return ExceptionMessages.NOT_ENOUGH_MONEY;
-    }
-
-    @Override
-    public void createErrorFile(String errorMessage) {
-        super.createErrorFile(errorMessage);
-    }
+public class NotEnoughMoneyException extends Exception {
 }
