a0b88a61011ab0473816db70f92799890b5e9934
Vlad.Zherko <vladikon19@tut.by>
2024-07-08 00:28:37 +0300

    sql scripts code added

--- src/main/java/ru/clevertec/check/utils/SQLCommands.java
+++ src/main/java/ru/clevertec/check/utils/SQLCommands.java
@@ -7,4 +7,11 @@ import lombok.NoArgsConstructor;
 public class SQLCommands {
     public static String DISCOUNT_BY_NUMBER = "SELECT * FROM public.discount_card WHERE number = ?";
     public static String PRODUCT_BY_ID = "SELECT * FROM public.product WHERE id = ?";
+    public static String DISCOUNT_CARD_BY_ID = "SELECT * FROM public.discount_card WHERE id = ?";
+    public static String SAVE_PRODUCT = "INSERT INTO public.product (description, price, quantity_in_stock, wholesale_product) VALUES (?,?,?,?)";
+    public static String DELETE_PRODUCT_BY_ID = "DELETE FROM public.product as p WHERE p.id = ?";
+    public static String FULL_PRODUCT_UPDATE = "UPDATE public.product SET description = ?, price = ?, quantity_in_stock = ?, wholesale_product = ? WHERE id = ?";
+    public static String SAVE_DISCOUNT_CARD = "INSERT INTO public.discount_card (number, amount) VALUES (?,?)";
+    public static String DELETE_DISCOUNT_CAR_BY_ID = "DELETE FROM public.discount_card as dc WHERE dc.id = ?";
+    public static String FULL_UPDATE_DISCOUNT_CARD = "UPDATE public.discount_card SET number = ?, amount = ? WHERE id = ?";
 }
