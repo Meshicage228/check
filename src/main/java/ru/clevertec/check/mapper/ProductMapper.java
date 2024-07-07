7e3603bfc1233e89c94750a39a3dde011723ac4f
Vlad.Zherko <vladikon19@tut.by>
2024-07-08 00:29:35 +0300

    unnecessary method removed

--- src/main/java/ru/clevertec/check/mapper/ProductMapper.java
+++ src/main/java/ru/clevertec/check/mapper/ProductMapper.java
@@ -1,12 +1,10 @@
 package ru.clevertec.check.mapper;
 
 import org.mapstruct.Mapper;
-import ru.clevertec.check.domain.Product;
+import ru.clevertec.check.entity.ProductEntity;
 import ru.clevertec.check.dto.ProductDto;
 
 @Mapper
 public interface ProductMapper {
-    ProductDto toDto(Product product);
-
-    Product toEntity(ProductDto product);
+    ProductDto toDto(ProductEntity product);
 }
