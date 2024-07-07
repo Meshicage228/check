34a7c98d635c22928619ee291ac101450ba81095
Vlad.Zherko <vladikon19@tut.by>
2024-07-08 00:28:21 +0300

    servlet crud support

--- src/main/java/ru/clevertec/check/servlets/discountCard/DiscountCardServlet.java
+++ src/main/java/ru/clevertec/check/servlets/discountCard/DiscountCardServlet.java
@@ -1,15 +1,18 @@
 package ru.clevertec.check.servlets.discountCard;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import ru.clevertec.check.dto.CardDto;
+import ru.clevertec.check.exceptions.ResourceNotFoundException;
 import ru.clevertec.check.factory.ServiceFactory;
 import ru.clevertec.check.service.CardService;
-
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.stream.Collectors;
+
+import static javax.servlet.http.HttpServletResponse.*;
 
 @WebServlet(name = "DiscountCard servlet",
         urlPatterns = "/discountcards")
@@ -18,28 +21,72 @@ public class DiscountCardServlet extends HttpServlet {
     private CardService cardService;
 
     @Override
-    public void init() throws ServletException {
+    public void init() {
         objectMapper = new ObjectMapper();
         cardService = ServiceFactory.createCardService();
     }
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         Integer id = Integer.valueOf(req.getParameter("id"));
+        CardDto byId = null;
+
+        try {
+            byId = cardService.getById(id);
+            resp.setStatus(SC_OK);
+            resp.setCharacterEncoding("utf-8");
+            resp.setContentType("application/json");
+            resp.getWriter().write(objectMapper.writeValueAsString(byId));
+        } catch (ResourceNotFoundException e) {
+            resp.setStatus(SC_NOT_FOUND);
+        } catch (Exception e){
+            resp.setStatus(SC_INTERNAL_SERVER_ERROR);
+        }
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String jsonString = req.getReader().lines().collect(Collectors.joining());
+        CardDto workoutDto = objectMapper.readValue(jsonString, CardDto.class);
+        // TODO : Validate value
+        try {
+            cardService.save(workoutDto);
+        } catch (Exception e) {
+            resp.setStatus(SC_INTERNAL_SERVER_ERROR);
+        }
+        resp.setStatus(SC_CREATED);
     }
 
     @Override
-    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPut(req, resp);
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        Integer id = Integer.valueOf(req.getParameter("id"));
+        try {
+            CardDto byId = cardService.getById(id);
+        } catch (ResourceNotFoundException e) {
+            resp.setStatus(SC_NOT_FOUND);
+        } catch (Exception e){
+            resp.setStatus(SC_INTERNAL_SERVER_ERROR);
+        }
+
+        String jsonString = req.getReader().lines().collect(Collectors.joining());
+        CardDto workoutDto = objectMapper.readValue(jsonString, CardDto.class);
+        // TODO : Validate value
+        cardService.fullUpdateCard(workoutDto, id);
+        resp.setStatus(SC_CREATED);
     }
 
     @Override
-    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doDelete(req, resp);
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) {
+        Integer id = Integer.valueOf(req.getParameter("id"));
+        try {
+            CardDto byId = cardService.getById(id);
+        } catch (ResourceNotFoundException e) {
+            resp.setStatus(SC_NOT_FOUND);
+        } catch (Exception e){
+            resp.setStatus(SC_INTERNAL_SERVER_ERROR);
+        }
+
+        cardService.deleteById(id);
+        resp.setStatus(SC_ACCEPTED);
     }
 }
--- src/main/java/ru/clevertec/check/servlets/product/ProductServlet.java
+++ src/main/java/ru/clevertec/check/servlets/product/ProductServlet.java
@@ -2,16 +2,18 @@ package ru.clevertec.check.servlets.product;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import ru.clevertec.check.dto.ProductDto;
+import ru.clevertec.check.exceptions.ResourceNotFoundException;
 import ru.clevertec.check.factory.ServiceFactory;
 import ru.clevertec.check.service.ProductService;
-import ru.clevertec.check.service.impl.ProductServiceImpl;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.stream.Collectors;
+
+import static javax.servlet.http.HttpServletResponse.*;
 
 @WebServlet(name = "Product servlet",
         urlPatterns = "/products")
@@ -26,27 +28,63 @@ public class ProductServlet extends HttpServlet {
     }
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
         Integer id = Integer.valueOf(req.getParameter("id"));
-        ProductDto byId = productService.getById(id);
-
-        resp.setStatus(HttpServletResponse.SC_OK);
-        resp.setCharacterEncoding("utf-8");
-        resp.getWriter().write(objectMapper.writeValueAsString(byId));
+        ProductDto byId = null;
+        try {
+            byId = productService.getById(id);
+            resp.setStatus(SC_OK);
+            resp.setCharacterEncoding("utf-8");
+            resp.setContentType("application/json");
+            resp.getWriter().write(objectMapper.writeValueAsString(byId));
+        } catch (ResourceNotFoundException e) {
+            resp.setStatus(SC_NOT_FOUND);
+        } catch (Exception e){
+            resp.setStatus(SC_INTERNAL_SERVER_ERROR);
+        }
     }
 
     @Override
-    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPut(req, resp);
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        Integer id = Integer.valueOf(req.getParameter("id"));
+        String jsonString = req.getReader().lines().collect(Collectors.joining());
+        ProductDto workoutDto = objectMapper.readValue(jsonString, ProductDto.class);
+        try {
+            ProductDto byId = productService.getById(id);
+            productService.fullUpdateProduct(workoutDto, id);
+            resp.setStatus(SC_CREATED);
+        } catch (ResourceNotFoundException e) {
+            resp.setStatus(SC_NOT_FOUND);
+        } catch (Exception e) {
+            resp.setStatus(SC_INTERNAL_SERVER_ERROR);
+        }
+        // TODO : Validate value
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String jsonString = req.getReader().lines().collect(Collectors.joining());
+        ProductDto workoutDto = objectMapper.readValue(jsonString, ProductDto.class);
+        // TODO : Validate value
+        try {
+            productService.save(workoutDto);
+        } catch (Exception e){
+            resp.setStatus(SC_INTERNAL_SERVER_ERROR);
+        }
+        resp.setStatus(SC_CREATED);
     }
 
     @Override
-    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doDelete(req, resp);
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) {
+        Integer id = Integer.valueOf(req.getParameter("id"));
+        try {
+            ProductDto byId = productService.getById(id);
+            productService.deleteById(id);
+            resp.setStatus(SC_ACCEPTED);
+        } catch (ResourceNotFoundException e) {
+            resp.setStatus(SC_NOT_FOUND);
+        } catch (Exception e) {
+            resp.setStatus(SC_INTERNAL_SERVER_ERROR);
+        }
     }
 }
