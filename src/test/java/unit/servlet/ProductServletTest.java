27164dcd71cc9ac8995f9bb0adfdd0c187a6ad3d
Vlad.Zherko <vladikon19@tut.by>
2024-08-23 15:14:59 +0300

    added : checking exceptions

--- src/test/java/unit/servlet/ProductServletTest.java
+++ src/test/java/unit/servlet/ProductServletTest.java
@@ -1,7 +1,9 @@
 package unit.servlet;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
@@ -9,6 +11,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import ru.clevertec.check.dto.ProductDto;
+import ru.clevertec.check.exceptions.BadRequestException;
+import ru.clevertec.check.exceptions.ResourceNotFoundException;
 import ru.clevertec.check.service.ProductService;
 import ru.clevertec.check.servlets.product.ProductServlet;
 import javax.servlet.http.HttpServletResponse;
@@ -17,7 +21,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.StringReader;
 
+import static javax.servlet.http.HttpServletResponse.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 @DisplayName("Product servlet tests")
@@ -87,5 +93,61 @@ class ProductServletTest extends AbstractServletTests{
     public BufferedReader createBufferReader(ProductDto userDto) throws IOException {
         String jsonString = new ObjectMapper().writer().withDefaultPrettyPrinter().writeValueAsString(userDto);
         return new BufferedReader(new StringReader(jsonString));
+    }
+
+    @Nested
+    @DisplayName("check exceptions")
+    public class CheckExceptions{
+
+        private ProductDto build;
+
+        @BeforeEach
+        void setUp() throws IOException {
+            build = ProductDto.builder()
+                    .quantity(12).build();
+
+            when(request.getParameter(anyString())).thenReturn("1");
+            when(request.getReader()).thenReturn(createBufferReader(build));
+            when(objectMapper.readValue(anyString(), eq(ProductDto.class))).thenReturn(build);
+        }
+
+        @Test
+        @DisplayName("doGet doDelete doPut : 404 - Not Found Exception")
+        public void resourceNotFound() throws Exception {
+            doNothing().when(validator).validateProductDto(any(ProductDto.class));
+            when(productService.getById(anyInt())).thenThrow(new ResourceNotFoundException());
+
+            productServlet.doGet(request, response);
+            productServlet.doPut(request, response);
+            productServlet.doDelete(request, response);
+
+            assertThrows(ResourceNotFoundException.class, () -> productService.getById(1));
+            verify(response, times(3)).setStatus(SC_NOT_FOUND);
+        }
+
+        @Test
+        @DisplayName("All methods : 500 - Internal Server Error")
+        public void checkInternalServerError() throws Exception {
+            doThrow(new RuntimeException()).when(validator).validateProductDto(any(ProductDto.class));
+            when(productService.getById(anyInt())).thenThrow(new RuntimeException());
+
+            productServlet.doGet(request, response);
+            productServlet.doPut(request, response);
+            productServlet.doDelete(request, response);
+            productServlet.doPost(request, response);
+
+            verify(response, times(4)).setStatus(SC_INTERNAL_SERVER_ERROR);
+        }
+
+        @Test
+        @DisplayName("doPost doPut : 400 - BadRequest")
+        public void testDoPost_BadRequestException() throws Exception {
+            doThrow(new BadRequestException()).when(validator).validateProductDto(any(ProductDto.class));
+
+            productServlet.doPost(request, response);
+            productServlet.doPut(request, response);
+
+            verify(response, times(2)).setStatus(SC_BAD_REQUEST);
+        }
     }
 }
